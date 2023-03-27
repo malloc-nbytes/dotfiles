adae114857e762aaa3210f16feb4892fe058888b
ZDH20 <77458179+ZDH20@users.noreply.github.com>
2023-03-26 22:06:17 -0500

    Update init.el

--- init.el
+++ init.el
@@ -1,8 +1,13 @@
+;; Get Melpa Repo.
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+(require 'smartparens-config)
+(require 'smartparens)
+
 ;; Vanilla settings.
+(show-paren-mode 1)
 (setq make-backup-files                nil)
 (setq display-line-numbers-type       'relative)
 (setq inhibit-startup-screen           t)
@@ -17,6 +22,7 @@
 (global-display-line-numbers-mode)
 (set-face-background 'trailing-whitespace "yellow")
 (set-frame-font "Fira Code-15")
+(windmove-default-keybindings)
 
 ;; Other variables.
 ;;; Good Scroll.
@@ -26,11 +32,18 @@
 (setq dired-listing-switches    "-alh")
 ;;; Auto Complete.
 (setq global-auto-complete-mode t)
+;;; Expand Region.
+(setq alphabet-start            "abc def")
+;;; Multiple Cursors.
+(setq mc/always-run-for-all     t)
+;;; Smart Parens.
+(smartparens-global-mode        t)
 
 ;; Keybind remaps.
 ;;; Emacs.
 (global-set-key (kbd "C-q")         'undo)
-(global-set-key (kbd "C-c c")       'compile)
+(global-set-key (kbd "C-;")         'previous-buffer)
+(global-set-key (kbd "C-'")         'next-buffer)
 (global-set-key (kbd "C-x m")       'delete-other-windows)
 (global-set-key (kbd "C-x '")       'shell)
 ;;; Crux.
@@ -62,12 +75,35 @@
 (global-set-key (kbd "C-c C-c")     'execute-extended-command)
 ;;; Imenu.
 (global-set-key (kbd "C-c f")       'imenu-list)
+;;; Auto Complete.
+(global-auto-complete-mode           1)
+;;; Expand Region.
+(global-set-key (kbd "C-.") 'er/expand-region)
 
 ;; Custom Functions.
 (add-hook 'emacs-lisp-mode-hook
           '(lambda ()
              (local-set-key (kbd "C-c C-j")
                             (quote eval-print-last-sexp))))
+
+(defun compile-and-switch-to-compilation-buffer ()
+  "Compile and switch to the compilation buffer, making it full-screen."
+  (interactive)
+  (setq compilation-read-command t)
+  (let ((compilation-buffer-name "*compilation*")
+        (buffer-window (selected-window)))
+    (call-interactively 'compile)
+    (delete-other-windows buffer-window)
+    (switch-to-buffer compilation-buffer-name)))
+(global-set-key (kbd "C-c c") 'compile-and-switch-to-compilation-buffer)
+
+(sp-local-pair 'prog-mode "{" nil :post-handlers '((my-create-newline-and-enter-sexp "RET")))
+(defun my-create-newline-and-enter-sexp (&rest _ignored)
+  "Open a new brace or bracket expression, with relevant newlines and indent. "
+  (newline)
+  (indent-according-to-mode)
+  (forward-line -1)
+  (indent-according-to-mode))
 
 ;; Other.
 (custom-set-variables
@@ -80,7 +116,7 @@
    '("bddf21b7face8adffc42c32a8223c3cc83b5c1bbd4ce49a5743ce528ca4da2b6" default))
  '(ispell-dictionary nil)
  '(package-selected-packages
-   '(good-scroll rust-mode imenu-list magit vterm crux gruber-darker-theme helm-swoop move-text multiple-cursors smex)))
+   '(smartparens expand-region auto-complete good-scroll rust-mode imenu-list magit vterm crux gruber-darker-theme helm-swoop move-text multiple-cursors smex)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
