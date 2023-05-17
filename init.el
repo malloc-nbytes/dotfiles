4744d67dbb12e1c32f4eb2f7332b7ca143f37709
malloc-nbytes <77458179+malloc-nbytes@users.noreply.github.com>
2023-05-17 17:19:25 -0500

    Update init.el

--- init.el
+++ init.el
@@ -6,107 +6,95 @@
 (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
                          ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
-
-(add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
-
 (require 'dired-x)
-(global-set-key (kbd "C-x C-j")         'dired-jump)
+(require 'dashboard)
 
+;; Misc. Config. ;;
+(setenv "PATH" (concat (getenv "PATH") ":/home/zdh/.cargo/bin"))
+(add-hook 'dired-mode-hook (lambda () (whitespace-mode -1)))
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
 (display-time-mode 1)
 (setq whitespace-style (quote (face spaces tabs space-mark tab-mark)))
 (global-whitespace-mode 1)
-
 (setq-default indent-tab-mode nil)
-
-(defun untabify-except-makefiles ()
-  "Replace tabs with spaces except in makefiles."
-  (unless (derived-mode-p 'makefile-mode)
-    (untabify (point-min) (point-max))))
-(add-hook 'before-save-hook 'untabify-except-makefiles)
-
-;; (setq split-height-threshold           9999)
-;; (setq split-width-threshold            nil)
-
-;; Vanilla settings.
 (show-paren-mode 1)
-(setq make-backup-files                nil)
-(setq display-line-numbers-type       'relative)
-(setq inhibit-startup-screen           t)
+(setq make-backup-files nil)
+(setq display-line-numbers-type 'relative)
+(setq inhibit-startup-screen t)
 (setq-default show-trailing-whitespace t)
-(scroll-bar-mode      -1)
-(ido-mode              1)
-(ido-everywhere        1)
-(ido-ubiquitous-mode   1)
-(menu-bar-mode         0)
-(tool-bar-mode         0)
+(scroll-bar-mode -1)
+(menu-bar-mode 0)
+(tool-bar-mode 0)
 (delete-selection-mode 1)
 (global-display-line-numbers-mode)
 (set-face-background 'trailing-whitespace "yellow")
 (set-frame-font "Fira Code-15")
 (windmove-default-keybindings)
 
-;; Other variables.
-;;; Good Scroll.
-(good-scroll-mode               1)
-;;; Dired.
+
+;; Package Config. ;;
+(dashboard-setup-startup-hook)
+(setq dashboard-banner-logo-title "www.gnu.org / Emacs version 27.1")
+(setq dashboard-startup-banner "~/Pictures/emacs_fancy_logos/xemacs_color.svg")
+(setq dashboard-footer-messages '("I showed you my code, pls respond (ﾉಥДಥ)ﾉ ︵┻━┻･/"))
+(setq dashboard-items nil)
+(ido-mode 1)
+(ido-everywhere 1)
+(good-scroll-mode 1)
 (setq-default dired-dwim-target t)
-(setq dired-listing-switches    "-alh")
-;;; Auto Complete.
+(setq dired-listing-switches "-alh")
 (setq global-auto-complete-mode t)
-;;; Expand Region.
-(setq alphabet-start            "abc def")
-;;; Multiple Cursors.
-(setq mc/always-run-for-all     t)
-;;; Smart Parens.
-(smartparens-global-mode        t)
-;;; Golden Ratio
+(setq mc/always-run-for-all t)
+(smartparens-global-mode t)
 (golden-ratio-mode)
+(global-auto-complete-mode 1)
 
-;; Keybind remaps.
-;;; Emacs.
-(global-set-key (kbd "C-q")         'undo)
-(global-set-key (kbd "C-;")         'previous-buffer)
-(global-set-key (kbd "C-'")         'next-buffer)
-(global-set-key (kbd "C-x m")       'delete-other-windows)
-(global-set-key (kbd "C-x '")       'shell)
 
-;;; Crux.
-(global-set-key (kbd "C-c e")       'crux-eval-and-replace)
-(global-set-key (kbd "C-c d")       'crux-duplicate-current-line-or-region)
-(global-set-key (kbd "C-c k")       'crux-kill-other-buffers)
-(global-set-key (kbd "M-j")         'crux-top-join-line)
-(global-set-key (kbd "C-h")         'crux-kill-whole-line)
-(global-set-key (kbd "C-x C-u")     'crux-upcase-region)
-(global-set-key (kbd "C-x C-l")     'crux-downcase-region)
-(global-set-key [(shift return)]    #'crux-smart-open-line)
-;;; Helm.
-(global-set-key (kbd "M-s M-s")     'helm-swoop)
-;;; Multiple Cursors.
+;; Keybindings. ;;
+(global-set-key (kbd "C-x C-j") 'dired-jump)
+(global-set-key (kbd "C-.") 'mark-word)
+(global-set-key (kbd "C-q") 'undo)
+(global-set-key (kbd "C-x m") 'delete-other-windows)
+(global-set-key (kbd "C-x '") 'shell)
+
+(global-set-key (kbd "C-c e") 'crux-eval-and-replace)
+(global-set-key (kbd "C-c d") 'crux-duplicate-current-line-or-region)
+(global-set-key (kbd "C-c k") 'crux-kill-other-buffers)
+(global-set-key (kbd "M-j") 'crux-top-join-line)
+(global-set-key (kbd "C-h") 'crux-kill-whole-line)
+(global-set-key (kbd "C-x C-u") 'crux-upcase-region)
+(global-set-key (kbd "C-x C-l") 'crux-downcase-region)
+(global-set-key [(shift return)] #'crux-smart-open-line)
+
+(global-set-key (kbd "M-s M-s") 'helm-swoop)
+
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C->")         'mc/mark-next-like-this)
-(global-set-key (kbd "C-<")         'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<")     'mc/mark-all-like-tnis)
-(global-set-key (kbd "C-?")         'mc/skip-to-next-like-this)
-;;; Move Text.
-(global-set-key (kbd "M-p")         'move-text-up)
-(global-set-key (kbd "M-n")         'move-text-down)
-;;; Split windows.
-(global-set-key (kbd "C-x -")       'split-window-vertically)
-(global-set-key (kbd "C-x /")       'split-window-horizontally)
-;;; Smex.
-(global-key-binding (kbd "M-x")     #'smex)
-(global-set-key (kbd "M-x")         'smex)
-(global-set-key (kbd "M-X")         'smex-major-mode-commands)
-;; (global-set-key (kbd "C-c C-l")     'execute-extended-command)
-;;; Imenu.
-(global-set-key (kbd "C-c f")       'imenu-list)
-;;; Auto Complete.
-(global-auto-complete-mode           1)
-;;; Expand Region.
-(global-set-key (kbd "C-.") 'er/expand-region)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-tnis)
+(global-set-key (kbd "C-?") 'mc/skip-to-next-like-this)
 
-;; Custom Functions.
+(global-set-key (kbd "M-p") 'move-text-up)
+(global-set-key (kbd "M-n") 'move-text-down)
+
+(global-set-key (kbd "C-x -") 'split-window-vertically)
+(global-set-key (kbd "C-x /") 'split-window-horizontally)
+
+(global-key-binding (kbd "M-x") #'smex)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c f") 'imenu-list)
+(global-set-key (kbd "C-=") 'er/expand-region)
+
+
+;; Functions. ;;
+(defun untabify-except-makefiles ()
+  "Replace tabs with spaces except in makefiles."
+  (unless (derived-mode-p 'makefile-mode)
+    (untabify (point-min) (point-max))))
+(add-hook 'before-save-hook 'untabify-except-makefiles)
+
 (add-hook 'emacs-lisp-mode-hook
           '(lambda ()
              (local-set-key (kbd "C-c C-j")
@@ -131,26 +119,3 @@
   (indent-according-to-mode)
   (forward-line -1)
   (indent-according-to-mode))
-
-
-;; Other.
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(gruber-darker))
- '(custom-safe-themes
-   '("bddf21b7face8adffc42c32a8223c3cc83b5c1bbd4ce49a5743ce528ca4da2b6" default))
- '(easy-repeat-command-list
-   '(other-window next-buffer scroll-other-window recenter-top-bottom kill-buffer backward-page forward-page previous-error next-error scroll-up-command scroll-down-command beginning-of-defun end-of-defun org-previous-visible-heading org-next-visible-heading org-forward-heading-same-level org-backward-heading-same-level outline-up-heading outline-next-visible-heading outline-previous-visible-heading outline-forward-same-level outline-backward-same-level git-gutter:previous-hunk git-gutter:next-hunk paredit-forward paredit-backward paredit-backward-up enlarge-window-horizontally shrink-window-horizontally enlarge-window))
- '(ido-max-prospects 25)
- '(ispell-dictionary nil)
- '(package-selected-packages
-   '(ido-completing-read+ golden-ratio smartparens expand-region auto-complete good-scroll rust-mode imenu-list magit vterm crux gruber-darker-theme helm-swoop move-text multiple-cursors smex)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
